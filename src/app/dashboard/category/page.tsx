61e287ccd38b5c3b3c1cdd2ab4a7eb9d4c904532
aleson01 <alesonsanley@gmail.com>
2024-11-24 11:08:00 -0400

    feat: cadastrar categorias no banco de dados

--- src/app/dashboard/category/page.tsx
+++ src/app/dashboard/category/page.tsx
@@ -1,13 +1,36 @@
 import styles from './styles.module.scss'
 import { Button } from "@/app/dashboard/components/button"
+import { api } from '@/services/api'
+import { redirect } from 'next/navigation'
+import {getCookieServer} from '@/lib/cookieServer'
 
 export default function Category(){
 
-  async function handleRegisterCategory(){
+  async function handleRegisterCategory(formData: FormData){
     "use server"
-    console.log("TESTEEE")
-  }
+    
+    const name = formData.get("name")
 
+    if(name === "") return;
+
+    const data = {
+      name: name,
+    }
+    const token =  await getCookieServer();
+
+    await api.post("/category", data, {
+      headers:{
+        Authorization: `Bearer ${token}`
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      return;
+    })
+
+    redirect("/dashboard")
+
+  }
 
   return(
     <main className={styles.container}>
