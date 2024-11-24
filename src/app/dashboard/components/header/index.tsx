a27b58f2bb7fa98f2ed51d5c46ae30400bb4cd51
aleson01 <alesonsanley@gmail.com>
2024-11-24 10:12:09 -0400

    feat: Criado logout

---
+++ src/app/dashboard/components/header/index.tsx
@@ -0,0 +1,51 @@
+"use client"
+import Link from 'next/link'
+import styles from './styles.module.scss'
+import Image from 'next/image'
+import logoImg from '/public/logopizzaria.png'
+import { LogOutIcon } from 'lucide-react'
+import { deleteCookie } from 'cookies-next'
+import { useRouter } from 'next/navigation'
+
+export function Header(){
+
+  const router = useRouter();
+
+  async function handleLogout(){
+    deleteCookie("session", { path: "/"})
+
+    router.replace("/")
+  }
+
+  return(
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <Link href="/dashboard">
+          <Image
+            alt="Logo Pizzaria"
+            src={logoImg}
+            width={60}
+            height={60}
+            priority={true}
+            quality={100}
+          />
+        </Link>
+
+        <nav>
+          <Link href="/dashboard/category">
+            Categoria
+          </Link>
+          <Link href="/dashboard/product">
+            Produto
+          </Link>
+
+          <form action={handleLogout}>
+            <button type='submit'>
+              <LogOutIcon size={24} color="#FFF" />
+            </button>
+          </form>
+        </nav>
+      </div>
+    </header>
+  )
+}
